13caadebb58cf4b88163d83e8f992e961bbeedc9
yuval savariego <yuvalego@gmail.com>
2025-05-02 04:38:16 +0300

    changed file name

--- App/start_server.sh
+++ App/start_server.sh
@@ -7,4 +7,4 @@ eval "$(/Users/yuvalsavaryegolandesman/miniconda3/bin/conda shell.bash hook)"
 conda activate ai
 
 # Run uvicorn
-uvicorn Image_to_Label_TFLite:app --host 0.0.0.0 --port 8000
+uvicorn InferanceAPI:app --host 0.0.0.0 --port 8000
